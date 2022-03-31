7c7b9ba0cf187fd033d216d21a177ca31b28d54b
Mehmet Furkan Adiguzel <55146992+Mehmetadiguzel1@users.noreply.github.com>
2022-03-31 10:58:57 -0500

    Added fetch fron api

--- app/pages/Home.js
+++ app/pages/Home.js
@@ -2,6 +2,27 @@ import React, { Component } from "react";
 import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 
 export default class Home extends Component {
+  _isMount = false;
+  state = {
+    isLoading: false,
+    recentMovies: [],
+    popularMovies: [],
+  };
+  componentDidMount() {
+    this._isMount = true;
+    return fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=def30dcb6753a6abdcf8682057b1ed85&language=en-US&page=1"
+    )
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson.results);
+      })
+      .catch((error) => console.error(error));
+  }
+  componentWillUnmount() {
+    this._isMount = false;
+  }
+
   render() {
     return (
       <SafeAreaView style={styles.container}>
